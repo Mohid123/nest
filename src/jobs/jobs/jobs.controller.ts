bac0d740f72d635896a2bbaa20570a4d734ba19c
Mohid <mohid.dastgeer@solissol.com>
2021-11-17 13:04:58 +0500

    Error Handling with HTTP Exception Filter

--- src/jobs/jobs/jobs.controller.ts
+++ src/jobs/jobs/jobs.controller.ts
@@ -1,4 +1,79 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Get, Post, Put, Delete, Body, Param, UseFilters } from '@nestjs/common';
+import { JobsService } from './jobs.service';
+import { JobDto } from './jobs.dto';
+import { HttpException } from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common';
+import { HttpExceptionFilter } from 'src/filters/http-exception.filter';
+
 
 @Controller('jobs')
-export class JobsController {}
+@UseFilters(HttpExceptionFilter)
+export class JobsController {
+    constructor(private readonly jobService: JobsService) { }
+
+    @Get('allJobs')
+    //@UseFilters(HttpExceptionFilter) //for error handling of entire file
+    async getAllJobs() {
+        const jobs = this.jobService.findAllJobs();
+        return await jobs.then((result) => {
+            if (result) {
+                return result
+            } else {
+                throw new HttpException('Jobs Not Found', HttpStatus.NOT_FOUND)
+            }
+        }).catch(() => {
+            throw new HttpException('Jobs Not Found', HttpStatus.NOT_FOUND)
+        });
+    }
+
+    @Get('oneJob/:id')
+    async getOneJob(@Param('id') id) {
+        const oneJob = this.jobService.findJob(id);
+        return await oneJob.then((result) => {
+            if (result) {
+                return result
+            }
+            else {
+                throw new HttpException('Job Not Found', HttpStatus.NOT_FOUND)
+            }
+        }).catch(() => {
+            throw new HttpException('Job Not Found', HttpStatus.NOT_FOUND)
+        });
+    }
+
+    @Post('newJob')
+    async postNewJob(@Body() jobsDto: JobDto) {
+        const newJob = this.jobService.postJob(jobsDto);
+        return newJob.then((result) => {
+            if (result) {
+                return result
+            }
+            else {
+                throw new HttpException('Cannot Post Job', HttpStatus.FORBIDDEN)
+            }
+        }).catch(() => {
+            throw new HttpException('Cannot Post Job', HttpStatus.FORBIDDEN)
+        });
+    }
+
+    @Put('updateJob/:id')
+    async updateJob(@Param('id') id, @Body() jobsDto: JobDto) {
+        return this.jobService.updateJob(id, jobsDto).then((result) => {
+            if (result) {
+                return result
+            }
+            else {
+                throw new HttpException('Cannot Update Job', HttpStatus.I_AM_A_TEAPOT)
+            }
+        }).catch(() => {
+            throw new HttpException('Cannot Update Job', HttpStatus.I_AM_A_TEAPOT)
+        });
+    }
+
+    @Delete('deleteJob/:id')
+    async deleteJob(@Param('id') id) {
+        return this.jobService.deleteJob(id);
+    }
+
+}
--- src/jobs/jobs/jobs.service.ts
+++ src/jobs/jobs/jobs.service.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { Injectable, NotAcceptableException, NotFoundException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { Job } from './jobs.interface';
@@ -9,17 +9,32 @@ import { JobDto } from './jobs.dto';
 export class JobsService {
     constructor(@InjectModel('Job') private readonly jobModel: Model<Job>) { }
 
-    async findAll() {
-        return await this.jobModel.find()
+    async findAllJobs() {
+        const allJobs = await this.jobModel.find().exec();
+        return allJobs as Job[]
     }
 
     async findJob(id: string) {
         return await this.jobModel.findOne({ _id: id })
     }
 
-    async updateJob(jobdto: JobDto) {
-        const jobDoc = this.jobModel.findOne({ title: jobdto.title });
-        if (jobDoc) {
+    async postJob(jobdto: JobDto) {
+        const newJob = await new this.jobModel(jobdto);
+        return newJob.save();
+    }
+
+    async updateJob(id, jobdto: JobDto) {
+        const JobTitle = this.jobModel.findOne({ _id: id });
+        if (JobTitle) {
+            const updatedJob = await this.jobModel.updateOne({ ...jobdto });
+            return updatedJob;
         }
+        else if (!JobTitle) {
+            throw new HttpException('ID Not Found', HttpStatus.NOT_FOUND);
+        }
+    }
+
+    async deleteJob(id) {
+        return await this.jobModel.findByIdAndRemove({ _id: id });
     }
 }
